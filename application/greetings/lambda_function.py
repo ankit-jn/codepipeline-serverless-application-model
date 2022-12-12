eae6c5064b005205826e8bf2abe8c8105aa92bb6
Ankit Jain <ankii.jain@gmail.com>
2022-12-12 11:22:51 +0530

    Fix issues with layers

--- application/greetings/lambda_function.py
+++ application/greetings/lambda_function.py
@@ -1,14 +1,15 @@
+import json
 from core.GetTime.time import TimeUtil
 
 def lambda_handler(event, context):
     time_util = TimeUtil()
 
-    now = time_util.get_time()
-    current_time = now.strftime("%H:%M:%S")
-    return {
-        "statusCode": 200,
-        "body": {
-            "message": "Hello Visitor!!!",
-            "time": "You visited us at {0}".format(current_time)
-        }
+    current_time = time_util.get_time()
+    response_message = {"Greetings": "Hello Visitor!!! You visited us at {0}".format(current_time)}
+    response = {
+    	"statusCode": 200,
+    	"isBase64Encoded": False,
+    	"headers": {},
+    	"body": json.dumps(response_message)
     }
+    return response
